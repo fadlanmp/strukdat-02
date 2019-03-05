1862dc3101b1a546f2a89b467efe2e6c2cc2ef44
fadlanmp <fadlanmulya@gmail.com>
2019-03-05 08:45:56 +0700

    pretest-2

---
+++ strukdat-02.cpp
@@ -0,0 +1,34 @@
+/*
+Nama Program		: 
+Nama				: Fadlan Mulya Priatna
+NPM					: 140810180041
+Tanggal Pembuatan	: 5 Maret 2019
+Deskripsi Program	: 
+*/
+
+#include <iostream>
+using namespace std;
+
+void moveZeroToEnd(int arr[],int n){
+	
+}
+void input(int (&arr)[100],int& n){
+	cout<<"Banyak data : ";cin>>n;
+	for (int i=0; i<n; i++){
+		cout<<"Data : "; cin>>data[i];
+	}
+}
+void output(int arr[],int n){
+	cout<<"Hasil : ";
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<",";
+	}
+	cout<<endl;
+}
+int main(){
+	int arr[100];
+	int n;
+	input(arr,n);
+	moveZeroToEnd(arr,n);
+	output(arr,n);
+}
